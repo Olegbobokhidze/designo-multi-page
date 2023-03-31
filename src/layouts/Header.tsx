a680725d3c171f8f6436febfffaa5cc1920476e0
Oleg Bobokhidze <oleg.bobokhidze.1@iliauni.edu.ge>
2023-03-31 18:40:53 +0400

    implemented hamburger logic

--- src/layouts/Header.tsx
+++ src/layouts/Header.tsx
@@ -1,18 +1,68 @@
-import React, { useState } from "react";
+import { Link } from "@tanstack/react-router";
+import React, { useRef, useState } from "react";
 import styled from "styled-components";
 import { LogoDark } from "../assets";
 import { Close, Hamburger } from "../svg";
 
 const Header = () => {
   const [showMenu, setShowMenu] = useState<boolean>(false);
+  const backDrop = useRef<HTMLDivElement | null>(null);
+
+  const backDropHandler: React.MouseEventHandler<HTMLDivElement> | undefined = (
+    event
+  ) => {
+    if (event.target === backDrop.current) {
+      setShowMenu(false);
+    }
+  };
   return (
     <HeaderComponent>
-      <Logo src={LogoDark} alt="logo" />
+      <Link to="/" onClick={() => setShowMenu(false)}>
+        <Logo src={LogoDark} alt="logo" />
+      </Link>
       {showMenu ? (
         <Close onClick={() => setShowMenu(false)} />
       ) : (
         <Hamburger onClick={() => setShowMenu(true)} />
       )}
+      {showMenu ? (
+        <Backdrop ref={backDrop} onClick={backDropHandler}>
+          <Menu>
+            <NavList>
+              <Link
+                to="/about"
+                onClick={() => setShowMenu(false)}
+                style={{
+                  textDecoration: "none",
+                  width: "fit-content",
+                }}
+              >
+                <NavItem>Our Company</NavItem>
+              </Link>
+              <Link
+                to="/location"
+                onClick={() => setShowMenu(false)}
+                style={{
+                  textDecoration: "none",
+                  width: "fit-content",
+                }}
+              >
+                <NavItem>locations</NavItem>
+              </Link>
+              <Link
+                to="/contact"
+                onClick={() => setShowMenu(false)}
+                style={{
+                  textDecoration: "none",
+                  width: "fit-content",
+                }}
+              >
+                <NavItem>contact</NavItem>
+              </Link>
+            </NavList>
+          </Menu>
+        </Backdrop>
+      ) : null}
     </HeaderComponent>
   );
 };
@@ -26,7 +76,36 @@ const HeaderComponent = styled.header`
   display: flex;
   justify-content: space-between;
   align-items: center;
+  position: relative;
 `;
 const Logo = styled.img`
   width: 202px;
+`;
+const Backdrop = styled.div`
+  width: 100vw;
+  height: calc(100vh - 96px);
+  position: absolute;
+  left: 0;
+  top: 96px;
+  background-color: rgba(0, 0, 0, 0.5);
+`;
+const Menu = styled.div`
+  width: 100%;
+  padding: 48px 24px;
+  background-color: black;
+`;
+const NavList = styled.ul`
+  display: flex;
+  flex-direction: column;
+  list-style: none;
+  row-gap: 32px;
+`;
+const NavItem = styled.h3`
+  font-weight: 400;
+  font-size: 24px;
+  line-height: 25px;
+  letter-spacing: 2px;
+  color: white;
+  text-transform: uppercase;
+  width: fit-content;
 `;
