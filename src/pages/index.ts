4806266dec50f021243bfcbf089ed8e7652e442a
Oleg Bobokhidze <oleg.bobokhidze.1@iliauni.edu.ge>
2023-03-31 16:59:04 +0400

    added pages and exports from index ts

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,2 +1,5 @@
 export { default as Home } from "./Home";
 export { default as About } from "./About";
+export { default as Location } from "./Location";
+export { default as Design } from "./Design";
+export { default as Contact } from "./Contact";
