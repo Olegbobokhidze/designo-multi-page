c1e48f044141c54c9fc2535a5b95c1864bd948cf
Oleg Bobokhidze <oleg.bobokhidze.1@iliauni.edu.ge>
2023-03-31 18:07:05 +0400

    added header comp

--- src/layouts/Header.tsx
+++ src/layouts/Header.tsx
@@ -1,7 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
+import styled from "styled-components";
+import { LogoDark } from "../assets";
+import { Close, Hamburger } from "../svg";
 
 const Header = () => {
-  return <div>Header</div>;
+  const [showMenu, setShowMenu] = useState<boolean>(false);
+  return (
+    <HeaderComponent>
+      <Logo src={LogoDark} alt="logo" />
+      {showMenu ? (
+        <Close onClick={() => setShowMenu(false)} />
+      ) : (
+        <Hamburger onClick={() => setShowMenu(true)} />
+      )}
+    </HeaderComponent>
+  );
 };
 
 export default Header;
+
+const HeaderComponent = styled.header`
+  width: 100%;
+  height: 96px;
+  padding: 0 24px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+const Logo = styled.img`
+  width: 202px;
+`;
