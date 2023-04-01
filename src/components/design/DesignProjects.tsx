43239f2d078585ec0aaea960fc25b3aeb7437204
Oleg Bobokhidze <oleg.bobokhidze.1@iliauni.edu.ge>
2023-04-01 18:00:00 +0400

    added design projects list

---
+++ src/components/design/DesignProjects.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import styled from "styled-components";
+import { ProjectType } from "../../types";
+import Project from "./Project";
+
+const DesignProjects: React.FC<{ projects: ProjectType[] }> = ({
+  projects,
+}) => {
+  return (
+    <List>
+      {projects.map((project) => (
+        <Project key={project.title} project={project} />
+      ))}
+    </List>
+  );
+};
+
+export default DesignProjects;
+
+const List = styled.ul`
+  display: grid;
+  width: 100%;
+  padding: 10px;
+  row-gap: 20px;
+  column-gap: 20px;
+  grid-template-columns: auto auto;
+`;
