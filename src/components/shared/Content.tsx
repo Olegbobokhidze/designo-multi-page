02322c08d68a202eb30434bb1cdf4de46772cc85
Oleg Bobokhidze <oleg.bobokhidze.1@iliauni.edu.ge>
2023-04-02 16:40:01 +0400

    implemented contents in about page

--- src/components/shared/Content.tsx
+++ src/components/shared/Content.tsx
@@ -1,7 +1,47 @@
 import React from "react";
-
-const Content = () => {
-  return <div>Content</div>;
+import styled, { css } from "styled-components";
+type PropsType = {
+  title: string;
+  image: string;
+  description: string;
+  color: string;
+};
+const Content: React.FC<PropsType> = ({ title, image, description, color }) => {
+  return (
+    <Wrapper color={color}>
+      <Img src={image} alt="contentImg" />
+      <Title color={color}>{title}</Title>
+      <Desc color={color}>{description}</Desc>
+    </Wrapper>
+  );
 };
 
 export default Content;
+
+const Wrapper = styled.div(
+  (props: { color: string }) => css`
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    gap: 20px;
+    align-items: center;
+    text-align: center;
+    background-color: ${props.color};
+  `
+);
+const Img = styled.img`
+  width: 100%;
+  height: 300px;
+`;
+const Title = styled.h3(
+  (props: { color: string }) => css`
+    color: ${props.color === "#E7816B" ? "white" : "black"};
+  `
+);
+
+const Desc = styled.p(
+  (props: { color: string }) => css`
+    color: ${props.color === "#E7816B" ? "white" : "black"};
+    padding: 40px;
+  `
+);
--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -1,11 +1,28 @@
 import React from "react";
-import { Countries } from "../../components";
-
+import styled from "styled-components";
+import { Content, Contents, Countries } from "../../components";
+import { DealMobile } from "../../assets";
 const About = () => {
+  const Desc =
+    "As strategic partners in our clients’ businesses, we are ready to take on any challenge as our own. Solving real problems require empathy and collaboration, and we strive to bring a fresh perspective to every opportunity. We make design and technology more accessible and give you tools to measure success.We are visual storytellers in appealing and captivating ways. By combining business and marketing strategies, we inspire audiences to take action and drive real results.";
   return (
     <>
+      <Contents />
       <Countries />
+      <Wrapper>
+        <Content
+          color="#FDF3F0"
+          description={Desc}
+          title="The Real Deal"
+          image={DealMobile}
+        />
+      </Wrapper>
     </>
   );
 };
 export default About;
+
+const Wrapper = styled.div`
+  margin-top: 100px;
+  width: 100%;
+`;
