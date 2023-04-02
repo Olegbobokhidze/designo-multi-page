94bb299e04822a7ceaecd637536780b8cde6fad7
Oleg Bobokhidze <oleg.bobokhidze.1@iliauni.edu.ge>
2023-04-02 16:15:29 +0400

    created single Content

--- src/components/shared/index.ts
+++ src/components/shared/index.ts
@@ -2,3 +2,4 @@ export { default as Project } from "./Project";
 export { default as Info } from "./info";
 export { default as Description } from "./Description";
 export { default as Country } from "./Country";
+export { default as Content } from "./Content";
