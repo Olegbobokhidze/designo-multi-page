5063fea068edc03b1b1635996974b731843325ed
Oleg Bobokhidze <oleg.bobokhidze.1@iliauni.edu.ge>
2023-04-02 15:53:36 +0400

    implemented single country style and path

--- src/components/shared/Country.tsx
+++ src/components/shared/Country.tsx
@@ -1,7 +1,49 @@
-import React from "react";
+import { Link } from "@tanstack/react-router";
+import styled, { css } from "styled-components";
 
-const Country = () => {
-  return <div>Country</div>;
+type PropsType = {
+  title: string;
+  image: string;
+};
+
+const Country: React.FC<PropsType> = ({ title, image }) => {
+  return (
+    <Main>
+      <Img alt="countryimg" src={image} />
+      <CountryTitle>{title}</CountryTitle>
+      <Link to="/location" style={{ textDecoration: "none" }}>
+        <Button>See Location</Button>
+      </Link>
+    </Main>
+  );
 };
 
 export default Country;
+
+const Main = styled.div`
+  width: 100%;
+  height: 250px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+const Img = styled.img`
+  width: 100%;
+  height: 200px;
+`;
+const CountryTitle = styled.h2`
+  font-weight: 500;
+  font-size: 28px;
+  line-height: 36px;
+  letter-spacing: 1.4px;
+  color: var(--light);
+  text-transform: uppercase;
+`;
+
+const Button = styled.button`
+  background-color: #e7816b;
+  border-radius: 8px;
+  width: 152px;
+  height: 56px;
+`;
