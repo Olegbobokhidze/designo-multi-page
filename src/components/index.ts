433ae5cb3df19066e4ddd9dc93095be5f6029ce8
Oleg Bobokhidze <oleg.bobokhidze.1@iliauni.edu.ge>
2023-04-02 15:47:20 +0400

    added country components and exports for each

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
 export * from "./home";
 export * from "./shared";
 export * from "./home";
+export * from "./about";
--- src/components/shared/index.ts
+++ src/components/shared/index.ts
@@ -1,3 +1,4 @@
 export { default as Project } from "./Project";
 export { default as Info } from "./info";
 export { default as Description } from "./Description";
+export { default as Country } from "./Country";
