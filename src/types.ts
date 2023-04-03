6a13c050b9311a1264323eb1e9632f4fc9cb4a09
Oleg Bobokhidze <oleg.bobokhidze.1@iliauni.edu.ge>
2023-04-03 13:26:53 +0400

    added required validation

--- src/types.ts
+++ src/types.ts
@@ -6,10 +6,12 @@ export type ProjectType = {
   category: string;
   image: string;
 };
-export const InvoiceType = z.object({
-  home: z.string(),
-  email: z.string(),
-  phone: z.string(),
-  message: z.string(),
-});
+export const InvoiceType = z
+  .object({
+    home: z.string().min(2),
+    email: z.string().min(2),
+    phone: z.string().min(2),
+    message: z.string().min(2),
+  })
+  .required();
 export type InfoSchemaType = z.infer<typeof InvoiceType>;
