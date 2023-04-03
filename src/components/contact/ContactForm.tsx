61bbb39250af475469398df9b414c144d7c5c6c0
Oleg Bobokhidze <oleg.bobokhidze.1@iliauni.edu.ge>
2023-04-03 13:12:52 +0400

    implemented type for validation and form

--- src/components/contact/ContactForm.tsx
+++ src/components/contact/ContactForm.tsx
@@ -1,30 +1,64 @@
 import React from "react";
 import styled from "styled-components";
-
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { InfoSchemaType, InvoiceType } from "../../types";
 const ContactForm = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    control,
+    watch,
+  } = useForm<InfoSchemaType>({
+    resolver: zodResolver(InvoiceType),
+    mode: "onChange",
+  });
+  const onSubmit = (data: InfoSchemaType) => {
+    console.log(data);
+  };
   return (
-    <Wrapper>
-      <Title>Contact Us</Title>
-      <Description>
-        Ready to take it to the next level? Let’s talk about your project or
-        idea and find out how we can help your business grow. If you are looking
-        for unique digital experiences that’s relatable to your users, drop us a
-        line.
-      </Description>
-      <Input placeholder="Home" />
-      <Input placeholder="Email Address" />
-      <Input placeholder="Phone" />
-      <TextArea placeholder="Your Message" />
-    </Wrapper>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <Wrapper>
+        <TextWrapper>
+          <Title>Contact Us</Title>
+          <Description>
+            Ready to take it to the next level? Let’s talk about your project or
+            idea and find out how we can help your business grow. If you are
+            looking for unique digital experiences that’s relatable to your
+            users, drop us a line.
+          </Description>
+        </TextWrapper>
+        <Input placeholder="Home" {...register("home")} />
+        <Input placeholder="Email Address" {...register("email")} />
+        <Input placeholder="Phone" {...register("phone")} />
+        <TextArea placeholder="Your Message" {...register("message")} />
+        <button>bbb</button>
+      </Wrapper>
+    </form>
   );
 };
 
 export default ContactForm;
 
+const TextWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  text-align: center;
+`;
 const Wrapper = styled.div`
   width: 100%;
+  height: 700px;
+  gap: 30px;
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+  align-items: center;
   padding: 20px;
   background-color: #e7816b;
+  margin-bottom: 70px;
 `;
 const Title = styled.h1`
   color: white;
@@ -40,24 +74,30 @@ const Description = styled.p`
 const Input = styled.input`
   width: 100%;
   height: 50px;
-  border-bottom: 1px solid whitesmoke;
   background-color: #e7816b;
   color: whitesmoke;
+  border: none;
+  border-bottom: 1px solid whitesmoke;
   padding-left: 10px;
   &:focus {
     outline: none;
-    border: 1px solid whitesmoke;
+  }
+  &::placeholder {
+    color: white;
   }
 `;
 const TextArea = styled.textarea`
   width: 100%;
   min-height: 100px;
+  border: none;
   border-bottom: 1px solid whitesmoke;
   background-color: #e7816b;
   color: whitesmoke;
-  padding-left: 10px;
+  padding: 10px;
   &:focus {
     outline: none;
-    border: 1px solid whitesmoke;
+  }
+  &::placeholder {
+    color: white;
   }
 `;
--- src/types.ts
+++ src/types.ts
@@ -1,6 +1,15 @@
+import * as z from "zod";
+
 export type ProjectType = {
   title: string;
   description: string;
   category: string;
   image: string;
 };
+export const InvoiceType = z.object({
+  home: z.string(),
+  email: z.string(),
+  phone: z.string(),
+  message: z.string(),
+});
+export type InfoSchemaType = z.infer<typeof InvoiceType>;
